d6dbb4e2448e25da974d22af3d87c629d61f5d7a
Kimiko <kimikoescovilla@gmail.com>
2021-11-15 19:23:53 +0900

    feat: added currstep without tts.

--- components/CurrentStep Components/TimerAndTTS.js
+++ components/CurrentStep Components/TimerAndTTS.js
@@ -1,124 +1,123 @@
-// import React, { useEffect, useState } from 'react';
-// import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+const formatNumber = number => `0${number}`.slice(-2);
 
-// const formatNumber = number => `0${number}`.slice(-2);
+const getRemaining = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time - minutes * 60;
+    return { minutes: formatNumber(minutes), seconds: formatNumber(seconds) };
+}
 
-// const getRemaining = (time) => {
-//     const minutes = Math.floor(time / 60);
-//     const seconds = time - minutes * 60;
-//     return { minutes: formatNumber(minutes), seconds: formatNumber(seconds) };
-// }
+const TimerAndTTS = ({step, instructions, time}) => {
+    const [remainingSecs, setRemainingSecs] = useState(time);
+    const [isActive, setIsActive] = useState(false);
+    const { minutes, seconds } = getRemaining(remainingSecs);
 
-// const TimerAndTTS = ({step, instructions, time}) => {
-//     const [remainingSecs, setRemainingSecs] = useState(time);
-//     const [isActive, setIsActive] = useState(false);
-//     const { minutes, seconds } = getRemaining(remainingSecs);
+    // const speak = () => {
+    //     say(`Step ${step}, ${instructions}`)
+    // }
 
-//     const speak = () => {
-//         say(`Step ${step}, ${instructions}`)
-//     }
-
-//     const toggleTimer = () => {
-//         setIsActive(!isActive);
-//       }
+    const toggleTimer = () => {
+        setIsActive(!isActive);
+      }
     
-//     const resetTimer = () => {
-//         setRemainingSecs(time);
-//         setIsActive(false);
-//     }
+    const resetTimer = () => {
+        setRemainingSecs(time);
+        setIsActive(false);
+    }
 
-//     const addTime = () => {
-//         setRemainingSecs(remainingSecs + 60);
-//     }
+    const addTime = () => {
+        setRemainingSecs(remainingSecs + 60);
+    }
 
-//     const subtractTime = () => {
-//         setRemainingSecs(remainingSecs - 60);
-//     }
+    const subtractTime = () => {
+        setRemainingSecs(remainingSecs - 60);
+    }
 
-//     useEffect(() => {
-//         if (remainingSecs === 0) {
-//             setIsActive(false)
-//             alert("Timer is done!")
-//         }
-//     }, [remainingSecs])
+    useEffect(() => {
+        if (remainingSecs === 0) {
+            setIsActive(false)
+            alert("Timer is done!")
+        }
+    }, [remainingSecs])
  
-//     useEffect(() => {
-//         let interval = null;
-//         if (isActive) {
-//             interval = setInterval(() => {
-//             setRemainingSecs(remainingSecs => remainingSecs - 1);
-//             }, 1000);
-//         } else if (!isActive && remainingSecs !== 0) {
-//             clearInterval(interval);
-//         }
-//         return () => clearInterval(interval);
-//     }, [isActive, remainingSecs]);
+    useEffect(() => {
+        let interval = null;
+        if (isActive) {
+            interval = setInterval(() => {
+            setRemainingSecs(remainingSecs => remainingSecs - 1);
+            }, 1000);
+        } else if (!isActive && remainingSecs !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isActive, remainingSecs]);
 
-//     return (
-//         <View style={{flex: 1, marginTop: 50 }}>
-//             <View style={{alignItems: 'center'}}>
-//                 <Text style={{fontSize: 70}}>{`${minutes}:${seconds}`}</Text>
-//                 <Text style={{fontSize: 20}}>minutes    seconds</Text>
-//             </View>
+    return (
+        <View style={{flex: 1, marginTop: 50 }}>
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontSize: 70}}>{`${minutes}:${seconds}`}</Text>
+                <Text style={{fontSize: 20}}>minutes    seconds</Text>
+            </View>
 
-//             <View style={{flex: 0.6, marginTop: 40}}>
-//                 <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', paddingLeft: 50, paddingRight: 50}}>
-//                     <TouchableOpacity style={styles.timerIcon} onPress={subtractTime}>
-//                         <Text style={{fontSize: 20}}> - </Text>
-//                     </TouchableOpacity>
-//                     <TouchableOpacity style={styles.adjustTimer} onPress={resetTimer}>
-//                         <Text style={{fontSize: 15, color: 'black'}}> Reset </Text>
-//                     </TouchableOpacity>
-//                     <TouchableOpacity style={styles.timerIcon} onPress={addTime}>
-//                         <Text style={{fontSize: 20}}> + </Text>
-//                     </TouchableOpacity>
-//                 </View>
-//                 <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
-//                     <TouchableOpacity style={styles.resumebtn} onPress={toggleTimer}>
-//                         { isActive ?  <Text style={{fontSize: 15}}> Pause </Text> :  <Text style={{fontSize: 15}}> Resume </Text> }
-//                     </TouchableOpacity>
-//                     <TouchableOpacity style={styles.tts} onPress={speak}>
-//                         <Text style={{fontSize: 15}}> Read Instructions </Text>
-//                     </TouchableOpacity>
-//                 </View>
-//             </View>
-//         </View>
-//     )
-// }
+            <View style={{flex: 0.6, marginTop: 40}}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', paddingLeft: 50, paddingRight: 50}}>
+                    <TouchableOpacity style={styles.timerIcon} onPress={subtractTime}>
+                        <Text style={{fontSize: 20}}> - </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.adjustTimer} onPress={resetTimer}>
+                        <Text style={{fontSize: 15, color: 'black'}}> Reset </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.timerIcon} onPress={addTime}>
+                        <Text style={{fontSize: 20}}> + </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 7 }}>
+                    <TouchableOpacity style={styles.resumebtn} onPress={toggleTimer}>
+                        { isActive ?  <Text style={{fontSize: 15}}> Pause </Text> :  <Text style={{fontSize: 15}}> Resume </Text> }
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tts}>
+                        <Text style={{fontSize: 15}}> Read Instructions </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
+}
 
-// const styles = StyleSheet.create({
-//     tts: {
-//         backgroundColor: 'white',
-//         padding: 25,
-//         alignItems: 'center',
-//         borderRadius: 60
-//     },
-//     resumebtn: {
-//         backgroundColor: 'white',
-//         padding: 25,
-//         paddingLeft: 50,
-//         paddingRight: 50,
-//         marginRight: 6,
-//         alignItems: 'center',
-//         borderRadius: 60,
-//     },
-//     adjustTimer: {
-//         backgroundColor: 'white',
-//         padding: 20,
-//         paddingLeft: 30,
-//         paddingRight: 30,  
-//         alignItems: 'center',
-//         borderRadius: 60,
-//     },
-//     timerIcon: {
-//         justifyContent: 'center',
-//         backgroundColor: '#9AD3BB',
-//         alignItems: 'center',
-//         width: 30,
-//         height: 30,
-//         borderRadius: 40
-//     }
-// });
+const styles = StyleSheet.create({
+    tts: {
+        backgroundColor: 'white',
+        padding: 25,
+        alignItems: 'center',
+        borderRadius: 60
+    },
+    resumebtn: {
+        backgroundColor: 'white',
+        padding: 25,
+        paddingLeft: 50,
+        paddingRight: 50,
+        marginRight: 6,
+        alignItems: 'center',
+        borderRadius: 60,
+    },
+    adjustTimer: {
+        backgroundColor: 'white',
+        padding: 20,
+        paddingLeft: 30,
+        paddingRight: 30,  
+        alignItems: 'center',
+        borderRadius: 60,
+    },
+    timerIcon: {
+        justifyContent: 'center',
+        backgroundColor: '#9AD3BB',
+        alignItems: 'center',
+        width: 30,
+        height: 30,
+        borderRadius: 40
+    }
+});
 
-// export default TimerAndTTS;
+export default TimerAndTTS;
--- components/CurrentStep.js
+++ components/CurrentStep.js
@@ -1,158 +1,146 @@
 import React, { useRef } from 'react'
-// import { Animated, StyleSheet, View, FlatList, Dimensions, Text, ScrollView } from 'react-native';
-// import { useRecoilState } from 'recoil';
-// import { stepsState } from '../atoms/Steps';
+import { Animated, StyleSheet, View, FlatList, Dimensions, Text, ScrollView } from 'react-native';
+import { useRecoilState } from 'recoil';
+import { stepsState } from '../atoms/Steps';
+import TimerAndTTS from './CurrentStep Components/TimerAndTTS';
 
-const CurrentStep = ( { navigation }) => {
-    return ( 
-        <View>
-            <Text>CurrentStep</Text>
+function CurrentStep() {
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const {width, height} = Dimensions.get('screen');
+    const [steps, setSteps] = useRecoilState(stepsState);
+    console.log(steps)
+
+    const bgs = ['#F5CA82', '#F4E7A0', '#E7B25A', '#E18B52', '#F5CA82'];
+
+    const Indicator=({scrollX}) => {
+        return <View style={{position: 'absolute', bottom: 80, flexDirection: 'row'}}>
+            {steps.map((_, i) => {
+                const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+                const scale = scrollX.interpolate({
+                    inputRange,
+                    outputRange: [0.8, 1.4, 0.8],
+                    extrapolate: 'clamp'
+        
+                });
+                const opacity = scrollX.interpolate({
+                    inputRange,
+                    outputRange: [0.6, 0.9, 0.6],
+                    extrapolate: 'clamp'
+        
+                });
+                return <Animated.View 
+                key={`indicator-${i}`}
+                style={{
+                    height: 10,
+                    width: 10,
+                    borderRadius: 5,
+                    backgroundColor: '#fff',
+                    opacity,
+                    margin: 5,
+                    transform: [
+                        {
+                            scale
+                        }
+                    ]
+                }}/>
+            })}
         </View>
-     );
-}
- 
-export default CurrentStep;
+    }
 
+    const Backdrop = ({scrollX}) => {
+        const backgroundColor = scrollX.interpolate({
+            inputRange: bgs.map((_, i) => i * width),
+            outputRange: bgs.map((bg) => bg)
 
-// import TimerAndTTS from './CurrentStep Components/TimerAndTTS';
+        });
+        return (
+        <Animated.View
+            style={[
+                StyleSheet.absoluteFillObject,
+            {
+                backgroundColor,
+            }
+        ]}/>
+        )
+    }
 
-// function CurrentStep() {
-//     const scrollX = useRef(new Animated.Value(0)).current;
-//     const {width, height} = Dimensions.get('screen');
-//     const [steps, setSteps] = useRecoilState(stepsState);
-//     console.log(steps)
+    const Square = ({scrollX}) => {
+        const YOLO = Animated.modulo(
+            Animated.divide(Animated.modulo(scrollX, width), new Animated.Value(width)), 1);
 
-//     const bgs = ['#F5CA82', '#F4E7A0', '#E7B25A', '#E18B52', '#F5CA82'];
-
-//     const Indicator=({scrollX}) => {
-//         return <View style={{position: 'absolute', bottom: 80, flexDirection: 'row'}}>
-//             {steps.map((_, i) => {
-//                 const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-//                 const scale = scrollX.interpolate({
-//                     inputRange,
-//                     outputRange: [0.8, 1.4, 0.8],
-//                     extrapolate: 'clamp'
+        const rotate = YOLO.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['35deg', '-35deg', '35deg']
+        })
         
-//                 });
-//                 const opacity = scrollX.interpolate({
-//                     inputRange,
-//                     outputRange: [0.6, 0.9, 0.6],
-//                     extrapolate: 'clamp'
-        
-//                 });
-//                 return <Animated.View 
-//                 key={`indicator-${i}`}
-//                 style={{
-//                     height: 10,
-//                     width: 10,
-//                     borderRadius: 5,
-//                     backgroundColor: '#fff',
-//                     opacity,
-//                     margin: 5,
-//                     transform: [
-//                         {
-//                             scale
-//                         }
-//                     ]
-//                 }}/>
-//             })}
-//         </View>
-//     }
-
-//     const Backdrop = ({scrollX}) => {
-//         const backgroundColor = scrollX.interpolate({
-//             inputRange: bgs.map((_, i) => i * width),
-//             outputRange: bgs.map((bg) => bg)
-
-//         });
-//         return (
-//         <Animated.View
-//             style={[
-//                 StyleSheet.absoluteFillObject,
-//             {
-//                 backgroundColor,
-//             }
-//         ]}/>
-//         )
-//     }
-
-//     const Square = ({scrollX}) => {
-//         const YOLO = Animated.modulo(
-//             Animated.divide(Animated.modulo(scrollX, width), new Animated.Value(width)), 1);
-
-//         const rotate = YOLO.interpolate({
-//             inputRange: [0, 0.5, 1],
-//             outputRange: ['35deg', '-35deg', '35deg']
-//         })
-        
-//         return <Animated.View
-//         style={{
-//             width: height, 
-//             height: height, 
-//             backgroundColor: 'white', 
-//             borderRadius: 86, 
-//             position: 'absolute',
-//             top: -height * 0.6,
-//             left: -height * 0.3,
-//             transform: [
-//                 {
-//                     rotate,
-//                 }
-//             ]
-//         }}/>
-//     }
+        return <Animated.View
+        style={{
+            width: height, 
+            height: height, 
+            backgroundColor: 'white', 
+            borderRadius: 86, 
+            position: 'absolute',
+            top: -height * 0.6,
+            left: -height * 0.3,
+            transform: [
+                {
+                    rotate,
+                }
+            ]
+        }}/>
+    }
     
-//     return (
-//         <View style={styles.container}>
-//             <Backdrop scrollX={scrollX}/>
-//             <Square scrollX={scrollX}/>
-//             <Animated.FlatList data={steps}
-//             keyExtractor={item => item.step}
-//             horizontal
-//             scrollEventThrottle={32}
-//             onScroll={Animated.event(
-//                 [{nativeEvent: {contentOffset: {x: scrollX}}}],
-//                 {useNativeDriver: false}
-//             )}
-//             contentContainerStyle={{paddingBottom: 100}}
-//             showsHorizontalScrollIndicator={false}
-//             pagingEnabled
-//             renderItem={({item}) => {
-//                 return (
-//                     <View style={{width, alignItems: 'center'}}>
-//                         <View style={{ flex: 0.3, justifyContent: 'center'}}>
-//                             <Text style={styles.stepInd}> Step { item.step } of {steps.length} </Text>
-//                         </View>
-//                         <View style={{ flex: 0.4 }}>
-//                             <ScrollView>
-//                             <Text style={styles.deets}> { item.details }</Text>
-//                             </ScrollView>
-//                         </View>
-//                         <TimerAndTTS step={item.step} instructions={item.details} time={item.timer}/>
-//                     </View>
-//                 )
-//             }}/>
-//             <Indicator scrollX={scrollX}/>
-//         </View>
-//     )
-// }
+    return (
+        <View style={styles.container}>
+            <Backdrop scrollX={scrollX}/>
+            <Square scrollX={scrollX}/>
+            <Animated.FlatList data={steps}
+            keyExtractor={item => item.step}
+            horizontal
+            scrollEventThrottle={32}
+            onScroll={Animated.event(
+                [{nativeEvent: {contentOffset: {x: scrollX}}}],
+                {useNativeDriver: false}
+            )}
+            contentContainerStyle={{paddingBottom: 100}}
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+            renderItem={({item}) => {
+                return (
+                    <View style={{width, alignItems: 'center'}}>
+                        <View style={{ flex: 0.3, justifyContent: 'center'}}>
+                            <Text style={styles.stepInd}> Step { item.step } of {steps.length} </Text>
+                        </View>
+                        <View style={{ flex: 0.4 }}>
+                            <ScrollView>
+                            <Text style={styles.deets}> { item.details }</Text>
+                            </ScrollView>
+                        </View>
+                        <TimerAndTTS step={item.step} instructions={item.details} time={item.timer}/>
+                    </View>
+                )
+            }}/>
+            <Indicator scrollX={scrollX}/>
+        </View>
+    )
+}
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         paddingTop: 40,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-//     stepInd: {
-//         fontSize: 35
-//     },
-//     deets: {
-//         fontSize: 20,
-//         marginBottom: 10,
-//         padding: 30
-//     }
-// });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    stepInd: {
+        fontSize: 35
+    },
+    deets: {
+        fontSize: 20,
+        marginBottom: 10,
+        padding: 30
+    }
+});
 
-// export default CurrentStep
+export default CurrentStep
