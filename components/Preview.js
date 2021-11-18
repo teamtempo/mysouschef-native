07dd9fe1b07fb52495d5d853365bb0f63a9ef594
Kimiko <kimikoescovilla@gmail.com>
2021-11-18 23:28:53 +0900

    feat: added an arrow for scroll.

--- components/Preview.js
+++ components/Preview.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Ingredients from './Preview/Ingredients';
 import Steps from './Preview/Steps';
 import { PorcupineManager } from '@picovoice/porcupine-react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 function Preview( { navigation } ) {
+    const scroll = useRef()
+
+    const scrollToBottom = () => {
+        scroll.current.scrollToEnd({animated: true})
+    }
 
     return (
     <View style={styles.container}>
         <View style={styles.main}>
             <Text style={styles.title}>Recipe</Text>
         </View>
-            <ScrollView showsVerticalScrollIndicator={true}>
+            <ScrollView
+            ref={scroll}
+            showsVerticalScrollIndicator={true}>
                 <Ingredients />
                 <Steps />
             </ScrollView>
+        <View style={styles.scrollView}>
+            <TouchableOpacity onPress={scrollToBottom}>
+                <Icon name="arrow-circle-down" size={55} color="#fff"/>
+            </TouchableOpacity>
+        </View>
         <View style={styles.main}>
             <TouchableOpacity onPress={() => navigation.navigate('CurrentStep')} style={styles.button}>
                 <Text style={{fontSize: 25, fontWeight: '900', color: 'white'}}>Start</Text>
@@ -55,6 +68,15 @@ const styles = StyleSheet.create({
     bgImage: {
         flex: 1,
         justifyContent: "center"
+    },
+    scrollView: {
+        position: 'absolute',
+        backgroundColor: "#9AD3BB",
+        alignItems: 'center',
+        borderRadius: 40,
+        width: 55,
+        bottom: 70,
+        right: 10,
     },
 
 });
