a874a4e9879b584337530073aa223984f35d1692
valch <valch161@gmail.com>
2021-11-18 19:12:56 +0900

    finished instructions

---
+++ atoms/ContinueClicked.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const continueClicked = atom({
+  key: 'continueClicked',
+  default: false,
+});
--- components/Preview.js
+++ components/Preview.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Steps from './Preview/Steps';
-import { PorcupineManager } from '@picovoice/porcupine-react-native';
+
+import { useRecoilState } from 'recoil';
+import { instructionsModal } from '../atoms/InstructionsModal';
+import Instructions from './Preview/Instructions';
+import { continueClicked } from '../atoms/ContinueClicked';
+
 
 function Preview( { navigation } ) {
 
+    const [modalVisible, setModalVisible] = useRecoilState(instructionsModal);
+    const [continueClickedState, setContinueClickedState] = useRecoilState(continueClicked);
+
+    useEffect(() => {
+        if (continueClickedState) {
+            navigation.navigate('CurrentStep');
+            setContinueClickedState(false);
+        }
+    }, [continueClickedState])
+
+    
+
     return (
+        
     <View style={styles.container}>
         <Text style={styles.title}>Recipe</Text>
         <Steps />
-        <TouchableOpacity onPress={() => navigation.navigate('CurrentStep')} style={styles.button}>
+        <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.button}>
             <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>Start</Text>
         </TouchableOpacity>
+        <Instructions continueClicked={continueClicked}/>
+        
+
     </View>
 
     );
@@ -37,14 +58,22 @@ const styles = StyleSheet.create({
         alignItems: "center",
         padding: 15,
     },
-    checkbox: {
-        alignItems: "center",
-        backgroundColor: "#D89B63",
-        padding: 10
-    },
     bgImage: {
         flex: 1,
         justifyContent: "center"
+    },
+    modal: {
+        flexDirection: 'column',
+        backgroundColor: "white", 
+        marginLeft:-15,
+        marginRight:-15,
+        paddingBottom: 10,
+        paddingTop: 10, 
+        paddingLeft: 2,
+        paddingRight: 10,
+        borderRadius:20, 
+        borderColor: '#F5B463',
+        borderWidth: 2,
     },
 
 });
