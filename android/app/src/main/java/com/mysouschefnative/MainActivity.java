1047c8bb12946c3ae2a08fd7dd705aeb8cdcb1c7
jamesbalcombe83 <jamesbalcombe83@gmail.com>
2021-12-08 14:02:27 +0900

    Fix: application naming@

--- android/app/src/main/java/com/mysouschefnative/MainActivity.java
+++ android/app/src/main/java/com/mysouschefnative/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mysouschefnative;
+package com.mysouschef;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -18,6 +18,6 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "mysouschefnative";
+    return "mysouschef";
   }
 }
--- ios/mysouschefnativeTests/mysouschefnativeTests.m
+++ ios/mysouschefnativeTests/mysouschefnativeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mysouschefnativeTests : XCTestCase
+@interface mysouschefTests : XCTestCase
 
 @end
 
-@implementation mysouschefnativeTests
+@implementation mysouschefTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
